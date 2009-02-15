c4f0441cc45c8d32b77366d8fe3137981c71de40
Trygve Vea <trygve.vea@gmail.com>
2009-02-15 19:45:36 +0100

    Added the GPL-notice, and fixed the header files.

--- src/highscores.h
+++ src/highscores.h
@@ -1,3 +1,23 @@
+/*
+    Awesome Tower Defense
+    Copyright (C) 2008-2009  Trygve Vea and contributors (read AUTHORS)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+#ifndef __HIGHSCORES_H__
+#define __HIGHSCORES_H__
 
 #define SCORE_NOMESSAGE     0
 #define SCORE_WONMESSAGE    1
@@ -5,3 +25,4 @@
 
 void show_highscores(int message);
 void draw_highscores(void);
+#endif /* __HIGHSCORES_H__ */
